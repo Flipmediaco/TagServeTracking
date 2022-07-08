9a7819a9da0add71b782636f3318ed456b794984
Laurie Greysky <lauriew@flipmedia.co.uk>
2022-07-08 13:57:10 +0100

    Implement Helper with Enable/Disable option

---
+++ Helper/Data.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Copyright © Laurie Greysky All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Flipmediaco\TagServeTracking\Helper;
+
+class Data extends \Magento\Framework\App\Helper\AbstractHelper
+{
+    /**
+     * @var \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+     */
+    protected $_storeScope;
+
+    /**
+    * @var \Magento\Framework\App\Config\ScopeConfigInterface
+    */
+    protected $_scopeConfig;
+
+    /**
+    * @var \Magento\Framework\Stdlib\CookieManagerInterface
+    */
+	protected $_cookieManager;
+
+    /**
+     * Data constructor.
+     * @param \Magento\Framework\App\Helper\Context $context
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Framework\Stdlib\CookieManagerInterface $cookieManager
+     */
+    public function __construct(
+        \Magento\Framework\App\Helper\Context $context,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+    	\Magento\Framework\Stdlib\CookieManagerInterface $cookieManager
+    ) {
+        $this->_storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
+        $this->_scopeConfig = $scopeConfig;
+        $this->_cookieManager = $cookieManager;
+
+        parent::__construct($context);
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isEnabled()
+    {
+        return ($this->_scopeConfig->getValue('tagserve/tagserve/enable', $this->_storeScope)) ? true : false;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMid()
+    {
+        return (int) $this->_scopeConfig->getValue('tagserve/tagserve/mid', $this->_storeScope);
+    }
+
+    /**
+     * @return int
+     */
+    public function getPid()
+    {
+        return (int) $this->_scopeConfig->getValue('tagserve/tagserve/pid', $this->_storeScope);
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrency()
+    {
+        return (int) $this->_scopeConfig->getValue('currency/options/base', $this->_storeScope);
+    }
+
+    /**
+     * @return string
+     */
+    public function getTagrid()
+    {
+        return ((int) $this->_cookieManager->getCookie('tagrid')) ? (int) $this->_cookieManager->getCookie('tagrid') : '';
+    }
+}
--- ViewModel/TagserveTrackingConversion.php
+++ ViewModel/TagserveTrackingConversion.php
@@ -10,14 +10,9 @@ namespace Flipmediaco\TagServeTracking\ViewModel;
 class TagserveTrackingConversion extends \Magento\Framework\DataObject implements \Magento\Framework\View\Element\Block\ArgumentInterface
 {
     /**
-    * @var \Magento\Framework\App\Config\ScopeConfigInterface
+    * @var \Flipmediaco\TagServeTracking\Helper\Data
     */
-    protected $_scopeConfig;
-
-    /**
-    * @var \Magento\Framework\Stdlib\CookieManagerInterface
-    */
-	protected $_cookieManager;
+    protected $_helper;
 	
     /**
     * @var \Magento\Sales\Model\Order
@@ -29,12 +24,10 @@ class TagserveTrackingConversion extends \Magento\Framework\DataObject implement
      *
      */
     public function __construct(
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-    	\Magento\Framework\Stdlib\CookieManagerInterface $cookieManager,
+        \Flipmediaco\TagServeTracking\Helper\Data $helper,
         \Magento\Sales\Model\Order $order
     ) {
-        $this->_scopeConfig = $scopeConfig;
-        $this->_cookieManager = $cookieManager;
+        $this->_helper = $helper;
         $this->_order = $order;
         parent::__construct();
     }
@@ -46,14 +39,20 @@ class TagserveTrackingConversion extends \Magento\Framework\DataObject implement
      *
      * @return string
      */
-    public function trackSale( $order_id ) {
-        // GET storeScope
-        $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
+    public function trackSale($order_id) {
+        // If isEnable is false
+        if (!$this->_helper->isEnabled()) return ""; 
+
+        // If order_id is false
+        if (!$order_id) return ""; 
         
-    	// Load order
-    	$order = $this->_order->loadByIncrementId($order_id);
-    
-    	// GET no_items
+    	// Load order from order_id
+    	$order = $this->_order->loadByIncrementId( $order_id );
+
+        // If order id is false
+        if (!$order->getId()) return ""; 
+
+    	// Set no_items
     	$no_items = 0;
         //
         // Get Items from Order
@@ -64,26 +63,31 @@ class TagserveTrackingConversion extends \Magento\Framework\DataObject implement
                 $no_items = $no_items + $item->getQtyOrdered();
             }
         }
-    	
-        // GET MID
-        $mid = (int) $this->_scopeConfig->getValue('tagserve/tagserve/mid', $storeScope);
+        // If no_items is false set to 1
+        if (!$no_items) $no_items = 1;
+
+        // Get order_amnt
+        $order_amnt = $order->getGrandTotal();
+
+        // Get mid
+        $mid = $this->_helper->getMid();
         
-        // GET PID
-        $pid = (int) $this->_scopeConfig->getValue('tagserve/tagserve/pid', $storeScope);
+        // Get pid
+        $pid = $this->_helper->getMid();
         
-        // GET currency
-        $currency = $this->_scopeConfig->getValue('currency/options/base', $storeScope);
+        // Get currency
+        $currency = $this->_helper->getCurrency();
+
+        // Get tagrid
+        $tagrid = $this->_helper->getTagrid();
         
-        // GET TagRid
-        $tagrid = ((int) $this->_cookieManager->getCookie('tagrid')) ? (int) $this->_cookieManager->getCookie('tagrid') : '';
-        
-        // Resturn Tracking pixel
+        // Return Tracking pixel
         return	'<img src="https://www.tagserve.com/saleServlet?' . 
         		'MID=' . $mid . '&' . 
         		'PID=' . $pid . '&' . 
         		'CRID=&' . 
         		'ORDERID=' . $order_id . '&' . 
-        		'ORDERAMNT=' . $order->getGrandTotal() . '&' . 
+        		'ORDERAMNT=' . $order_amnt . '&' . 
         		'NUMOFITEMS=' . $no_items . '&' . 
         		'CUR=' . $currency . '&' . 
         		'RID=' . $tagrid . '" style="display: none;">';
